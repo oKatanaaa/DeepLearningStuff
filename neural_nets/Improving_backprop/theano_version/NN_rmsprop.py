15133c8af117d9bad2b94a9d042d334ae4b351b3
oKatanaaa <whitemarsstudios@gmail.com>
2019-02-26 19:26:03 +0400

    Added theano code

---
+++ neural_nets/Improving_backprop/theano_version/NN_rmsprop.py
@@ -0,0 +1,137 @@
+import theano.tensor as T
+import numpy as np
+import theano
+from theano.tensor.nnet import relu
+from theano.tensor.nnet import softmax
+from sklearn.utils import shuffle
+from utils import error_rate
+import matplotlib.pyplot as plt 
+from utils import create_graph
+from utils import one_hot_encoding
+
+class TheanoNN(object):
+    
+    def __init__(self, D, H, K):
+        W1_init = np.random.randn(D, H) / np.sqrt(D)
+        b1_init = np.zeros(H)
+        W2_init = np.random.randn(H, K) / np.sqrt(H)
+        b2_init = np.zeros(K)
+        
+        self.W1 = theano.shared(W1_init, 'W1')
+        self.b1 = theano.shared(b1_init, 'b1')
+        self.W2 = theano.shared(W2_init, 'W2')
+        self.b2 = theano.shared(b2_init, 'b2')
+        
+        # Define holders for Input-X and Targets-T
+        self.thX = T.matrix('X')
+        self.thT = T.matrix('T')
+        
+        # Define layer outputs
+        self.thZ = relu(self.thX.dot(self.W1) + self.b1)
+        self.thY = softmax(self.thZ.dot(self.W2) + self.b2)
+        
+        # Define cost function
+        self.cost = -(self.thT * T.log(self.thY)).sum()
+        self.prediction = T.argmax(self.thY, axis=1)
+        
+        # Define function which is helpful for the training
+        self.get_prediction = theano.function(
+            inputs=[self.thX, self.thT],
+            outputs=[self.cost, self.prediction]
+        )
+
+    
+    def fit(self, Xtrain, Ytrain, Xtest, Ytest, epochs=1000, learning_rate=0.00001, batch_sz=100, reg=0.0, decay=0.999):
+        # Create caches
+        cache_W1 = theano.shared(np.zeros_like(self.W1.get_value()))
+        cache_b1 = theano.shared(np.zeros_like(self.b1.get_value()))
+        cache_W2 = theano.shared(np.zeros_like(self.W2.get_value()))
+        cache_b2 = theano.shared(np.zeros_like(self.b2.get_value()))
+        
+        updated_cache_W1 = decay*cache_W1 + (1 - decay)*T.grad(self.cost, self.W1)**2
+        updated_cache_b1 = decay*cache_b1 + (1 - decay)*T.grad(self.cost, self.b1)**2
+        updated_cache_W2 = decay*cache_W2 + (1 - decay)*T.grad(self.cost, self.W2)**2
+        updated_cache_b2 = decay*cache_b2 + (1 - decay)*T.grad(self.cost, self.b2)**2
+        
+        # Create epsilons to avoid division by zero
+        eps_W1 = theano.shared(np.ones_like(self.W1.get_value())/10e8)
+        eps_b1 = theano.shared(np.ones_like(self.b1.get_value())/10e8)
+        eps_W2 = theano.shared(np.ones_like(self.W2.get_value())/10e8)
+        eps_b2 = theano.shared(np.ones_like(self.b2.get_value())/10e8)
+        
+        
+        updated_W1 = self.W1 - learning_rate*T.grad(self.cost, self.W1) / T.sqrt(eps_W1 + updated_cache_W1)
+        updated_b1 = self.b1 - learning_rate*T.grad(self.cost, self.b1) / T.sqrt(eps_b1 + updated_cache_b1)
+        updated_W2 = self.W2 - learning_rate*T.grad(self.cost, self.W2) / T.sqrt(eps_W2 + updated_cache_W2)
+        updated_b2 = self.b2 - learning_rate*T.grad(self.cost, self.b2) / T.sqrt(eps_b2 + updated_cache_b2)
+        
+        train = theano.function(
+            inputs=[self.thX, self.thT],
+            updates=[
+                # Updated caches
+                (cache_W1, updated_cache_W1), (cache_b1, updated_cache_b1), 
+                (cache_W2, updated_cache_W2), (cache_b2, updated_cache_b2),
+                # Update weights
+                (self.W1, updated_W1), (self.b1, updated_b1), (self.W2, updated_W2), (self.b2, updated_b2)]
+        )
+        
+        n_batches = len(Xtrain) // batch_sz
+        # Create this variable in order to get cost per vector
+        cost_divider = len(Xtest)
+        
+        costs = []
+        errors = []
+        for i in range(epochs):
+            Xtrain, Ytrain = shuffle(Xtrain, Ytrain)
+            for j in range(n_batches):
+                Xbatch = Xtrain[j*batch_sz:(j+1)*batch_sz]
+                Ybatch = Ytrain[j*batch_sz:(j+1)*batch_sz]
+                
+                train(Xbatch, Ybatch)
+                cost, Yp = self.get_prediction(Xtest, Ytest)
+                error = error_rate(Yp, np.argmax(Ytest, axis=1))
+                
+                costs.append(cost/cost_divider)
+                errors.append(error)
+                if j % 10 == 0:
+                    print('Accuracy: ', 1 - error)
+                    print('Cost: ', cost/cost_divider)
+                    
+        return costs, errors
+        
+        
+def main():
+    Nclass = 500
+    D = 2
+    X1 = np.random.randn(Nclass, D) + np.array([0, 2])
+    X2 = np.random.randn(Nclass, D) + np.array([-2, 2])
+    X3 = np.random.randn(Nclass, D) + np.array([-2, 0])
+    X = np.vstack([X1, X2, X3])
+    
+    Y = np.array([0]*Nclass + [1]*Nclass + [2]*Nclass)
+    Y = one_hot_encoding(Y)
+    
+    
+    N = 1500
+    H = 10
+    K = 3
+    
+    Xtrain = X[:-100]
+    Ytrain = Y[:-100]
+    Xtest = X[-100:]
+    Ytest = Y[-100:]
+    
+    model = TheanoNN(D, H, K)
+    costs, errors = model.fit(Xtrain, Ytrain, Xtest, Ytest, epochs=1000)
+    
+    create_graph(costs, 'cost.png', y_label='cost', x_label='iteration')
+    create_graph(errors, 'error.png', y_label='error', x_label='iteration')
+
+if __name__ == '__main__':
+    main()
+
+        
+        
+        
+        
+        
---
+++ neural_nets/Improving_backprop/theano_version/test.py
@@ -0,0 +1,80 @@
+from utils import get_preprocessed_data
+import matplotlib.pyplot as plt
+from NN_vanilla import TheanoNN as VanillaNN
+from NN_momentum import TheanoNN as MomentumNN
+from NN_nesterov import TheanoNN as NMomentumNN
+#from NN_adagrad import TheanoNN as AdagradNN
+from NN_rmsprop import TheanoNN as RMSpropNN
+from NN_adam import TheanoNN as AdamNN
+#from NN_rmsmomentum import TheanoNN as RMSMomentumMM
+
+def main():
+    X, Y = get_preprocessed_data()
+    N, D = X.shape
+    # Number of hidden neurons
+    H = 100
+    K = Y.shape[1]
+    
+    # Split data
+    Xtrain = X[:-1000]
+    Ytrain = Y[:-1000]
+    
+    Xtest = X[-1000:]
+    Ytest = Y[-1000:]
+    
+    # NN with vanilla GD
+    #model1 = VanillaNN(D, H, K)
+    #_, v_error = model1.fit(Xtrain, Ytrain, Xtest, Ytest,
+    #                     epochs=100, learning_rate=0.00001, batch_sz=500)
+    
+    # NN with momentum GD
+    #model2 = MomentumNN(D, H, K)
+    #_, m_error = model2.fit(Xtrain, Ytrain, Xtest, Ytest,
+    #                     epochs=100, learning_rate=0.00001, batch_sz=500)
+    
+    # NN with Nesterov momentum GD
+    model3 = NMomentumNN(D, H, K)
+    _, nm_error = model3.fit(Xtrain, Ytrain, Xtest, Ytest,
+                         epochs=100, learning_rate=0.00001, batch_sz=500)
+    
+    # NN with Adagard
+    #model4 = AdagradNN(D, H, K)
+    #ag_error = model4.fit(Xtrain, Ytrain, Xtest, Ytest,
+    #                     epochs=100, learning_rate=0.001, batch_sz=500)
+    
+    # NN with RMSprop
+    model5 = RMSpropNN(D, H, K)
+    _, rms_error = model5.fit(Xtrain, Ytrain, Xtest, Ytest,
+                         epochs=100, learning_rate=0.001, batch_sz=500, decay=0.999)
+    
+    # NN with Adam
+    model6 = AdamNN(D, H, K)
+    _, am_error = model6.fit(Xtrain, Ytrain, Xtest, Ytest,
+                         epochs=100, learning_rate=0.001, batch_sz=500)
+    
+    # NN with custom algorithm
+    #model7 = RMSMomentumMM(D, H, K)
+    #cs_error = model7.fit(Xtrain, Ytrain, Xtest, Ytest,
+    #                    epochs=80, learning_rate=0.001, batch_sz=500)
+    
+    # Create error graphs
+    fig = plt.figure()
+    fig.set_size_inches(18.5, 10.5)
+    
+    axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
+    #axes.plot(v_error, label='Vanilla')
+    #axes.plot(m_error, label='Momentum')
+    #axes.plot(nm_error, label='Nesterov momentum')
+    #axes.plot(ag_error, label='Adagrad')
+    axes.plot(rms_error, label='RMSprop')
+    axes.plot(am_error, label='Adam')
+    #axes.plot(cs_error, label='Custom algorithm')
+    axes.legend()
+    
+    axes.set_ylabel('Error')
+    axes.set_xlabel('Iteration')
+    
+    fig.savefig('Test4_adam_rmsprop.png')
+    
+if __name__ == '__main__':
+    main()
